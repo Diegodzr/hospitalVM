bfc238ebfb0aee5719575f858a61bcf808bc11d3
Cetecom <benjamingaticatrejo123@gmail.com>
2025-05-07 16:25:02 -0400

    hice los package y objeto Paciente completado

--- hospital-vm/src/main/java/model/Paciente.java
+++ hospital-vm/src/main/java/model/Paciente.java
@@ -13,6 +13,21 @@ import java.util.Date;
 @AllArgsConstructor
 public class Paciente {
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @Column(unique = true,length = 13, nullable = false)
+    private String run;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable=true)
+    private Date fechaNacimiento;
+
+    @Column(nullable = false)
+    private String correo;
 }
